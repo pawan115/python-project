8213e7ec4047d59051075d6934aac391e9e9aed4
pawan115 <impawan18@gmail.com>
2017-08-15 22:09:07 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print 'lets get\'s started'
+raw_input("what is your name?")
+spy_name - raw_input("what is your name?")
+spam = 42
+print 'Welcome ' + spy_name + '. Glad to have you back with us.'
+
+
+>>> for temp
